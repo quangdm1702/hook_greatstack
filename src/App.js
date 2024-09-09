852de1f788fd97976da6969a61c20150165c098d
quangdm1702 <doanminhquang17022003@gmail.com>
2024-09-09 20:09:06 +0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('GreatStack');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount(count => count + 1);
+    }, 2000)
+  }, [count, name])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I have rendered {count} times!</h1>
     </div>
   );
 }
